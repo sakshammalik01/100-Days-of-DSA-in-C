1ffbc1180a33fd0ec5775be18f6aca9272c89f78
sakshammalik01 <sakshammalik0011@gmail.com>
2026-04-01 09:04:04 +0530

    Day 24: Delete first occurrence of a key in a linked list (C)

---
+++ day24.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    int n,x,key;
+    struct node *head=NULL,*temp=NULL,*newnode,*prev=NULL;
+
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&x);
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->data=x;
+        newnode->next=NULL;
+
+        if(head==NULL){
+            head=newnode;
+            temp=head;
+        }else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+
+    scanf("%d",&key);
+
+    temp=head;
+
+    while(temp!=NULL){
+        if(temp->data==key){
+            if(prev==NULL){
+                head=temp->next;
+            }else{
+                prev->next=temp->next;
+            }
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+
+    temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+
+    return 0;
+}
