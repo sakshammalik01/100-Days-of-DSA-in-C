b50aa73b53ba17c6a25b4cc8a88c98766ee4ec8a
sakshammalik01 <sakshammalik0011@gmail.com>
2026-04-01 09:09:09 +0530

    Day 26: Doubly Linked List insertion and traversal in C

---
+++ day26.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+int main(){
+    int n,x;
+    struct node *head=NULL,*temp=NULL,*newnode;
+
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&x);
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->data=x;
+        newnode->next=NULL;
+        newnode->prev=NULL;
+
+        if(head==NULL){
+            head=newnode;
+            temp=head;
+        }else{
+            temp->next=newnode;
+            newnode->prev=temp;
+            temp=newnode;
+        }
+    }
+
+    temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+
+    return 0;
+}
